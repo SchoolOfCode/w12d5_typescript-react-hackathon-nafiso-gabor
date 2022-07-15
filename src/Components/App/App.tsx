be7eeeef662787ebf9dbd2182eba8f24f2f17556
Nafiso <nafisoaden@Nafisos-MacBook-Pro.local>
2022-07-15 13:36:34 +0100

    passing props to weather component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,12 +1,40 @@
-import React from "react";
-import Search from "../Search/Search";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Weather from "../Weather/Weather";
 
 function App() {
-  return <div className="App">
-    <h1>Weather app</h1>
-    
-  </div>;
+  const [latitude, setLatitude] = useState<any>([]);
+  const [longitude, setLongitude] = useState<any>([]);
+  const [data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLatitude(30);
+        setLongitude(15);
+      });
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+          //console.log(data);
+        });
+    };
+    fetchData();
+  }, [latitude, longitude]);
+
+  // type WeatherProps = {
+  //   weatherdata: any;
+  // };
+  return (
+    <div className="App">
+      <Weather weatherData={data} />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Weather/Weather.tsx
+++ src/Components/Weather/Weather.tsx
@@ -1,12 +1,14 @@
-// import React, { useEffect, useState } from "react";
-import './App.css';
+import React from "react";
+//import "./styles.css";
+import { Card } from "semantic-ui-react";
+//import "./App.css";
 
-export default function Weather() {
+export default function Weather(weatherData: any) {
   return (
-    <div>
-<h1>Hello</h1>
-
-
-    </div>
-  )
+    <Card>
+      <Card.Content>
+        <Card.Header className="header">{weatherData}</Card.Header>
+      </Card.Content>
+    </Card>
+  );
 }
